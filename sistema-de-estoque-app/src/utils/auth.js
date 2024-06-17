0278ab38f7b575e27fd73780ec4d244248c4c92b
renan <renan1504@gmail.com>
2024-06-17 16:04:35 -0300

    auth utils logando usuário

--- sistema-de-estoque-app/src/utils/auth.js
+++ sistema-de-estoque-app/src/utils/auth.js
@@ -1,4 +1,16 @@
 import {createUserWithEmailAndPassword, getAuth, sendEmailVerification, signInWithEmailAndPassword} from "firebase/auth";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAapYTFKjrbV-aDPeS37OailcH4QPkkXtY",
+    authDomain: "sistema-de-estoque-d6464.firebaseapp.com",
+    projectId: "sistema-de-estoque-d6464",
+    storageBucket: "sistema-de-estoque-d6464.appspot.com",
+    messagingSenderId: "254882219928",
+    appId: "1:254882219928:web:75a1cb8a5e975e0ea64ded"
+  };
+  
+const app = initializeApp(firebaseConfig);
 
 const auth = getAuth();
 
@@ -18,16 +30,23 @@ export async function createUser(email, password){
 }
 
 export async function signIn(email, password){
-
-    return;
-    signInWithEmailAndPassword(auth, email, password)
+    await signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             
             const user = userCredential.user;
             if(user.emailVerified){
-                // go to dashboard page
+                let userStr = JSON.stringify(user);
+                window.localStorage.setItem("user", userStr);
             }else{
-                sendEmailVerification(user);
+                console.log("enviando email")
+                sendEmailVerification(user).then(
+                    onfulfilled((a)=>{                
+                        console.log("Fulfilled: " + a);
+                    }), 
+                    onrejected((a)=>{
+                        console.log("Rejected: " + a);
+                    })
+                );
             }
             console.log(user);
             // ...
@@ -37,4 +56,10 @@ export async function signIn(email, password){
             const errorMessage = error.message;
         });
     console.log("Logou com " + email + "e " + password);
+}
+
+export default function isLogged(){
+    console.log("User: " + window.localStorage.getItem("user"));
+    console.log(window.localStorage.getItem("user") != null);
+    return window.localStorage.getItem("user") != null;
 }
