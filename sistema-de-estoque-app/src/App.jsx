9db4106ffae10983ee044531bc3433f62394871c
renan <renan1504@gmail.com>
2024-06-17 16:03:18 -0300

    adicionando firebase ao projeto

--- sistema-de-estoque-app/src/App.jsx
+++ sistema-de-estoque-app/src/App.jsx
@@ -1,26 +1,30 @@
-import { lazy, useState } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './App.css'
-import { initializeApp } from "firebase/app";
+import Dashboard from './pages/dashboard';
+import Login from './pages/authentication/login';
 
-// import Login from './pages/authentication/login';
 
-const Login = lazy(()=> import("./pages/authentication/login"));
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAapYTFKjrbV-aDPeS37OailcH4QPkkXtY",
-  authDomain: "sistema-de-estoque-d6464.firebaseapp.com",
-  projectId: "sistema-de-estoque-d6464",
-  storageBucket: "sistema-de-estoque-d6464.appspot.com",
-  messagingSenderId: "254882219928",
-  appId: "1:254882219928:web:75a1cb8a5e975e0ea64ded"
-};
-const app = initializeApp(firebaseConfig);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  }, 
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+]);
 
 function App() {
-
   return (
     <>
-      <Login></Login>
+      <RouterProvider router={router}></RouterProvider>
     </>
   )
 }
