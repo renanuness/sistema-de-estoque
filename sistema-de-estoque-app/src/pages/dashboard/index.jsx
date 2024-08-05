fd3ad0aec897052adfec92f4b31b06dd8e33286f
renanuness <renan1504@gmail.com>
2024-08-05 10:01:47 -0300

    add dashboard buttons

--- sistema-de-estoque-app/src/pages/dashboard/index.jsx
+++ sistema-de-estoque-app/src/pages/dashboard/index.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import isLogged from "../../utils/auth";
 import { useEffect } from "react";
 import CreateProduct from "../products/create";
+import './styles.css';
 
 export default function Dashboard() {
     const navigate = useNavigate();
@@ -12,8 +13,24 @@ export default function Dashboard() {
             }
     })
 
-    return <>
-        <p>Dashboard</p>
-        <CreateProduct></CreateProduct>
-    </>
+    function redirect(dest){
+        alert(dest);
+    }
+
+    return (
+        <div className="dashboard">
+            <div onClick={()=>redirect("estoque")}>
+                <p>Estoque</p>
+            </div>
+            <div onClick={()=>redirect("funcionarios")}>
+                <p>Funcionários</p>
+            </div>
+            <div onClick={()=>redirect("relatorios")}> 
+                <p>Relatórios</p>
+            </div>
+            <div onClick={()=>redirect("vendas")}> 
+                <p>Venda</p>
+            </div>
+        </div>
+    );
 }
