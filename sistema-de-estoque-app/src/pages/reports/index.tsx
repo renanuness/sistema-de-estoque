158f5abd0b934af64002ee641dd56185c16646be
renan <renan1504@gmail.com>
2024-09-16 07:32:16 -0300

    login page fix

--- sistema-de-estoque-app/src/pages/reports/index.tsx
+++ sistema-de-estoque-app/src/pages/reports/index.tsx
@@ -1,6 +1,37 @@
-export default function Reports(){
+import { BarChart, PieChart } from '@mui/x-charts';
+import * as React from 'react';
 
-    return (<>
-        <p>Reports</p>
-    </>)
+export default function Reports() {
+
+  return (
+    <div>
+      <div>
+        <h1>Vendas por mês</h1>
+        <BarChart
+          xAxis={[{ scaleType: 'band', data: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'] }]}
+          series={[{ data: [15000, 18000.50, 21000.75, 19500, 22000, 25000, 24000.30, 27000.10, 26000, 28000.75, 30000.50, 35000] }]}
+          width={830}
+          height={400}
+        />
+      </div>
+      <div>
+        <h1>Vendas por categoria</h1>
+        <PieChart
+          series={[
+            {
+              data: [
+                { id: 0, value: 10, label: 'Beleza' },
+                { id: 1, value: 15, label: 'Móveis' },
+                { id: 2, value: 56, label: 'Smartphones' },
+                { id: 3, value: 20, label: 'Relógios' },
+                { id: 4, value: 40, label: 'Calçados' },
+              ],
+            },
+          ]}
+          width={400}
+          height={200}
+        />
+      </div>
+    </div>
+  )
 }
