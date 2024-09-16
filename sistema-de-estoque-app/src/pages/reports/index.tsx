51939f3f816693e8b09d7ec7a212e0eebe4b5c1e
renan <renan1504@gmail.com>
2024-09-16 08:55:57 -0300

    reports

--- sistema-de-estoque-app/src/pages/reports/index.tsx
+++ sistema-de-estoque-app/src/pages/reports/index.tsx
@@ -1,22 +1,26 @@
 import { BarChart, PieChart } from '@mui/x-charts';
 import * as React from 'react';
+import styles from './styles.module.css';
 
 export default function Reports() {
 
+
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <div className={styles.monthContainer}>
         <h1>Vendas por mês</h1>
         <BarChart
           xAxis={[{ scaleType: 'band', data: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'] }]}
           series={[{ data: [15000, 18000.50, 21000.75, 19500, 22000, 25000, 24000.30, 27000.10, 26000, 28000.75, 30000.50, 35000] }]}
           width={830}
           height={400}
+          className={styles.barChart}
         />
       </div>
-      <div>
+      <div className={styles.categoryContainer}>
         <h1>Vendas por categoria</h1>
         <PieChart
+          className={styles.pieChart}
           series={[
             {
               data: [
