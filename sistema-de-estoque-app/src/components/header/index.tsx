b5032f4b75fdaf880f0d8b4fe838dbdd2ef2c25b
renan <renan1504@gmail.com>
2024-09-16 09:03:01 -0300

    header styles

--- sistema-de-estoque-app/src/components/header/index.tsx
+++ sistema-de-estoque-app/src/components/header/index.tsx
@@ -1,16 +1,16 @@
-
-
+import styles from './styles.module.css';
 
 export default function Header(){
     
     return(
-        <div>
+        <div className={styles.container}>
             <nav>
                 <ul>
                     <li>Produtos</li>
                     <li>Funcionários</li>
                     <li>Vendas</li>
                     <li>Relatórios</li>
+                    <li>Logout</li>
                 </ul>
             </nav>
         </div>
